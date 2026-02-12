d0f6d831a859b995f3bac0ea316c2e8702117521
Lituh <liituh@gmail.com>
2026-02-12 10:58:42 -0300

    TC08

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,18 +4,19 @@ describe("Login User", () => {
 
     //     Confirmamos que estamos en la página del login, ingresamos datos de usuario registrado:
 
-       cy.visit("https://automationexercise.com")
-       cy.contains("Full-Fledged practice website for Automation Engineers").should("be.visible")
+    cy.visit("https://automationexercise.com")
+    cy.contains("Full-Fledged practice website for Automation Engineers").should("be.visible")
 
-       cy.get('a[href="/login"]').click()
+    cy.get('a[href="/login"]').click()
 
-       cy.contains("Login to your account").should("be.visible")
+    cy.contains("Login to your account").should("be.visible")
 
-       cy.get('.login-form').within(() => {
-       cy.get('input[name="email"]').type("ana123@test.com")
-       cy.get('[data-qa="login-password"]').type("ANA12345!")
-       cy.get('button[data-qa="login-button"]').click()
+    cy.get('.login-form').within(() => {
+    cy.get('input[name="email"]').type("ana123@test.com")
+    cy.get('[data-qa="login-password"]').type("ANA12345!")
+    cy.get('button[data-qa="login-button"]').click()
     })
+    
 //------------------------------------------------------------------------------------------------------------------//
 
     //     Confirmamos que estamos loggeados con nuestra cuenta con credenciales válidas
--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -2,6 +2,31 @@ describe("Products Page", () => {
 
   it("TC-AUTO-08 - Verify All Products and Product Detail Page", () => {
 
+    //     Confirmamos que estamos en la página del login y hacemos click en "Productos"
 
+    cy.visit("https://automationexercise.com")
+    cy.contains("Full-Fledged practice website for Automation Engineers").should("be.visible")
+
+    cy.get('a[href="/products"]').click()
+
+    //     Confirmamos que estamos en la página de los productos:
+
+    cy.contains("All Products").should("be.visible")
+    cy.get('.product-image-wrapper').should("have.length.greaterThan", 0) 
+
+    //     Hacemos click en el detalle del primer producto y verificamos la info de esa pantalla
+
+    cy.get('a[href*="/product_details/"]').first().click()
+
+    cy.get('.product-information').within(() => {
+
+      cy.get("h2").should("be.visible") 
+      cy.contains("Category:").should("be.visible") 
+      cy.contains("Rs.").should("be.visible") 
+      cy.contains("Availability:").should("be.visible")
+      cy.contains("Condition:").should("be.visible")
+      cy.contains("Brand:").should("be.visible")
+
+    })
   })
 })
